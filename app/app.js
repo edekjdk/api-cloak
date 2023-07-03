180c222d973253c7b02d1a24647ddb4a5db4b11f
Kacper <luszczkacper42@gmail.com>
2023-07-03 22:09:37 +0200

    refactoring js file

--- app/app.js
+++ app/app.js
@@ -1,7 +1,5 @@
-// const worldTimeAPI = 'https://worldtimeapi.org/api/timezone';
 const ipBaseAPI =
   'https://api.ipbase.com/v2/info?apikey=n2PKHxJs6IOE6W6HmT39RT4SlmZeVd9xMDFxStiY';
-let currLocation;
 const randonQuoteAPI = 'https://api.quotable.io/random';
 const quoute = document.querySelector('.quoute');
 const cloakCon = document.querySelector('.cloak');
@@ -10,10 +8,31 @@ const quouteText = document.querySelector('.quoute__text');
 const quouteAuthor = document.querySelector('.quoute__author');
 const quouteRefresh = document.querySelector('.quoute__refresh');
 const moreInformation = document.querySelector('.more-information');
+const testTime = document.querySelector('.test');
+const zone = document.querySelector('.cloak__cloak-zone');
+const place = document.querySelector('.cloak__container-place');
+const cloakHeaderImage = document.querySelector('.cloak__header-image');
+const cloakHeaderHeading = document.querySelector('.cloak__header-heading');
+const background1 = document.querySelector('.b1');
+const background2 = document.querySelector('.b2');
+const background3 = document.querySelector('.b3');
+const moreInformationZone = document.querySelector(
+  '.more-information__container-data-zone'
+);
+const moreInformationDayOfYear = document.querySelector(
+  '.more-information__container-data-day-of-year'
+);
+const moreInformationDayOfWeek = document.querySelector(
+  '.more-information__container-data-day-of-week'
+);
+const moreInformationWeekNumber = document.querySelector(
+  '.more-information__container-data-week-number'
+);
 let time;
 let city;
 let country;
-const testTime = document.querySelector('.test');
+let currLocation;
+
 const getTimeZone = (call) => {
   // fetch(ipBaseAPI)
   //   .then((res) => res.json())
@@ -37,68 +56,32 @@ const getTimeZone = (call) => {
 };
 
 const renderPage = (res) => {
-  const zone = document.querySelector('.cloak__cloak-zone');
-  console.log(res);
-
   zone.innerHTML = res.abbreviation;
-  document.querySelector('.more-information__container-data-zone').innerHTML =
-    res.timezone;
-  document.querySelector(
-    '.more-information__container-data-day-of-year'
-  ).innerHTML = res.day_of_year;
-  document.querySelector(
-    '.more-information__container-data-day-of-week'
-  ).innerHTML = res.day_of_week;
-  document.querySelector(
-    '.more-information__container-data-week-number'
-  ).innerHTML = res.week_number;
-  // place.innerHTML = res.time.slice();
+  moreInformationZone.innerHTML = res.timezone;
+  moreInformationDayOfYear.innerHTML = res.day_of_year;
+  moreInformationDayOfWeek.innerHTML = res.day_of_week;
+  moreInformationWeekNumber.innerHTML = res.week_number;
 };
 
 const changeTheme = (hours) => {
   if (hours >= 8 && hours < 20) {
     moreInformation.classList.remove('more-information--dark');
-    document
-      .querySelector('.cloak__header-image')
-      .setAttribute('src', 'assets/desktop/icon-sun.svg');
-    document.querySelector(
-      '.cloak__header-heading'
-    ).innerText = `good morning, it's currently`;
-    console.log(hours);
-    document
-      .querySelector('.b1')
-      .setAttribute('src', 'assets/mobile/bg-image-daytime.jpg');
-    document
-      .querySelector('.b2')
-      .setAttribute('src', 'assets/tablet/bg-image-daytime.jpg');
-    document
-      .querySelector('.b3')
-      .setAttribute('src', 'assets/desktop/bg-image-daytime.jpg');
+    cloakHeaderImage.setAttribute('src', 'assets/desktop/icon-sun.svg');
+    cloakHeaderHeading.innerText = `good morning, it's currently`;
+    background1.setAttribute('src', 'assets/mobile/bg-image-daytime.jpg');
+    background2.setAttribute('src', 'assets/tablet/bg-image-daytime.jpg');
+    background3.setAttribute('src', 'assets/desktop/bg-image-daytime.jpg');
   } else {
-    document
-      .querySelector('.cloak__header-image')
-      .setAttribute('src', 'assets/desktop/icon-moon.svg');
-
-    document.querySelector(
-      '.cloak__header-heading'
-    ).innerText = `good evening, it's currently`;
-
+    cloakHeaderImage.setAttribute('src', 'assets/desktop/icon-moon.svg');
+    cloakHeaderHeading.innerText = `good evening, it's currently`;
     moreInformation.classList.add('more-information--dark');
-    document
-      .querySelector('.b1')
-      .setAttribute('src', 'assets/mobile/bg-image-nighttime.jpg');
-    document
-      .querySelector('.b2')
-      .setAttribute('src', 'assets/tablet/bg-image-nighttime.jpg');
-    document
-      .querySelector('.b3')
-      .setAttribute('src', 'assets/desktop/bg-image-nighttime.jpg');
+    background1.setAttribute('src', 'assets/mobile/bg-image-nighttime.jpg');
+    background2.setAttribute('src', 'assets/tablet/bg-image-nighttime.jpg');
+    background3.setAttribute('src', 'assets/desktop/bg-image-nighttime.jpg');
   }
 };
 
 const renderPage2 = (res, res2) => {
-  // console.log(res);
-  const place = document.querySelector('.cloak__container-place');
   // place.innerHTML = `in ${res.data.location.city.name}, ${res.data.location.country.alpha2}`;
   place.innerHTML = `in ${res}, ${res2}`;
 };
@@ -106,21 +89,10 @@ const renderPage2 = (res, res2) => {
 const showLocation = (call) => {
   console.log(currLocation);
   let worldTimeAPI = `https://worldtimeapi.org/api/timezone/${currLocation}`;
-  // let worldTimeAPI = `http://worldtimeapi.org/api/timezone/America`;
   fetch(worldTimeAPI)
     .then((res) => res.json())
     .then((res) => {
-      // console.log(res.datetime);
-      console.log(res);
-      // console.log(res.datetime.slice(11, 19));
       time = res.datetime.slice(11, 19);
-      // console.log(time);
-      // console.log(country);
-      console.log(res.abbreviation);
-      // console.log(res.day_of_week);
-      // console.log(res.day_of_year);
-      // console.log(res.week_number);
-      // console.log(res.datetime.slice(0, 10));
       call();
       return res;
     })
@@ -131,10 +103,7 @@ const showLocation = (call) => {
 
 const cloak = () => {
   let seconds = Number(time.slice(-2));
-  // let seconds = 55;
-  // let minutes = 59;
   let minutes = Number(time.slice(3, 5));
-  // let hours = 7;
   let hours = Number(time.slice(0, 2));
   changeTheme(hours);
   setInterval(() => {
@@ -151,7 +120,6 @@ const cloak = () => {
         }
       }
     }
-    // console.log(seconds);
     testTime.innerHTML = `${hours}:${minutes}`;
     if (minutes < 10) {
       testTime.innerHTML = `${hours}:0${minutes}`;
@@ -166,41 +134,7 @@ const cloak = () => {
       }
     }
   }, 1000);
-
-  // setInterval(() => {
-  //   seconds += 1;
-  //   if (seconds === 60) {
-  //     seconds = 0;
-  //     minutes += 1;
-  //     if (minutes === 60) {
-  //       hours += 1;
-  //       minutes = 0;
-  //       if (hours === 24) {
-  //         hours = 0;
-  //       }
-  //     }
-  //   }
-  //   if (minutes < 10) {
-  //     testTime.innerHTML = `${hours}:0${minutes}`;
-  //     if (hours < 10) {
-  //       testTime.innerHTML = `0${hours}:0${minutes}`;
-  //     } else {
-  //       testTime.innerHTML = `${hours}:0${minutes}`;
-  //     }
-  //   } else {
-  //     testTime.innerHTML = `${hours}:${minutes}`;
-  //     if (hours < 10) {
-  //       testTime.innerHTML = `0${hours}:${minutes}`;
-  //     } else {
-  //       testTime.innerHTML = `${hours}:${minutes}`;
-  //     }
-  //   }
-  // }, 1000);
 };
-
-// setInterval(() => {
-//   showLocation();
-// }, 60000);
 
 getTimeZone(() => {
   showLocation(() => {
@@ -211,7 +145,6 @@ getTimeZone(() => {
 const newQuoute = () => {
   fetch(randonQuoteAPI)
     .then((res) => res.json())
-    // .then((res) => console.log(res))
     .then((res) => {
       quouteText.textContent = res.content;
       quouteAuthor.textContent = res.author;
@@ -222,7 +155,6 @@ quouteRefresh.addEventListener('click', newQuoute);
 window.addEventListener('load', newQuoute);
 
 const showMore = () => {
-  // console.log('jebac patricka cwela');
   quoute.classList.toggle('quoute--active');
   cloakCon.classList.toggle('cloak--active');
   moreInformation.classList.toggle('more-information--active');
@@ -232,7 +164,6 @@ const showMore = () => {
   } else {
     showMoreButton.innerHTML =
       '<h6>more</h6><div class="show-more-button__circle"><i class="fa-solid fa-angle-down" style="color: #ffffff;"></i></div>';
-    // '<h6>more</h6><div class="show-more-button__circle"><img src="assets/desktop/icon-arrow-down.svg" alt="arrowdown"></div>';
   }
 };
 
